59ab2e63f4e17785169663084b5ee7e7fec355be
Nikhil. R <rnikhil.2014@gmail.com>
2016-02-16 20:31:07 +0530

    adding petitions works with auth

--- main.php
+++ main.php
@@ -12,7 +12,6 @@ $app->contentType('application/json');
 
 
 $app->post('/', function() use ($app) {
-session_start(); // Starting Session
 
 $username = $app->request->post('username');
 $password = $app->request->post('password');
@@ -24,10 +23,10 @@ $article = R::findOne('user', 'username=?', array((string)$username));
       $pass_request = (string)$password;
       if($pass_db === $pass_request)
       {
-     
+     session_start(); // Starting Session
+
         $_SESSION['id'] = $article->id;
         $_SESSION['username'] = $article->username;
-        echo $_SESSION['username'];
         $app->redirect('posts.html');
           }
       else
@@ -52,13 +51,14 @@ $password = $app->request->post('password');
     $article->password = (string)$password;
     $article->username = (string)$username;
     $id = R::store($article);
-echo "registerd";
-$app->redirect('/add');
+echo "registerd. Now go login";
+// $app->redirect('/add');
 });
 
-
+$app->get('/add', function() use ($app){$app->redirect('posts.html');});
 
 $app->post('/add', function() use ($app) {
+session_start(); // Starting Session
 
 if(isset($_SESSION['username'])){
 
@@ -70,7 +70,9 @@ $details = $app->request->post('details');
     $article->password = (string)$title;
     $article->username = (string)$details;
     $id = R::store($article);
-  }else{$app->redirect('/login')}
+    echo "petition added";
+
+  }else{echo "login first ";}
 
 });
 
