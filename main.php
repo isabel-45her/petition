7e5577ee52502bf2d5a0d4f4457c5093a8506f52
Nikhil. R <rnikhil.2014@gmail.com>
2016-02-16 19:11:34 +0530

    Register working

--- main.php
+++ main.php
@@ -2,14 +2,9 @@
 
 require '/./libs/Slim/Slim.php';
 require '/./libs/rb.php';
-include "routes/login.php";
-include "routes/register.php";
-include "routes/petitions.php";
- 
 \Slim\Slim::registerAutoloader();
 
-// set up database connection
-R::setup('mysql:host=localhost;dbname=doctornowv1','root','');
+R::setup('mysql:host=localhost;dbname=petition','root','');
 
 //R::freeze(true);
 $app = new \Slim\Slim();
@@ -19,13 +14,51 @@ $app->contentType('application/json');
 $app->post('/', function() use ($app) {
 session_start(); // Starting Session
 
-$result = $app->request->post('username');
+$username = $app->request->post('username');
+$password = $app->request->post('password');
+
 echo $result;
+$article = R::findOne('user', 'username=?', array((string)$username));
+    if ($article) { // if found, return JSON response
+      $pass_db = (string)$article->password;
+      $pass_request = (string)$password;
+      if($pass_db === $pass_request)
+      {
+        $arr=array('status' => 'true', 'message' => 'logged in','patient_id' => $article->id); // store the id in front
+        $app->response()->header('Content-Type', 'application/javascript');
+        $msg=json_encode($arr );
+        $app->response->body($msg );
+        $_SESSION['id'] = $article->id;
+        $_SESSION['username'] = $article->username;
+          }
+      else
+      {
+        echo "wrong password";
+      }
+
+    }
+    else {
+echo "go register first";
+    }
+});
 
 
 
+$app->post('/register', function() use ($app) {
+
+$username = $app->request->post('username');
+$password = $app->request->post('password');
+    
+    $article = R::dispense('user');
+    $article->password = (string)$password;
+    $article->username = (string)$username;
+    $id = R::store($article);
+echo "registerd";
 
 });
+
+
+
 
 
 $app->run();
