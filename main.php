735805d5293e4715a1f3a6b59344377220fa5dec
Nikhil. R <rnikhil.2014@gmail.com>
2016-02-16 19:14:11 +0530

    login and register both working

--- main.php
+++ main.php
@@ -17,19 +17,17 @@ session_start(); // Starting Session
 $username = $app->request->post('username');
 $password = $app->request->post('password');
 
-echo $result;
+
 $article = R::findOne('user', 'username=?', array((string)$username));
     if ($article) { // if found, return JSON response
       $pass_db = (string)$article->password;
       $pass_request = (string)$password;
       if($pass_db === $pass_request)
       {
-        $arr=array('status' => 'true', 'message' => 'logged in','patient_id' => $article->id); // store the id in front
-        $app->response()->header('Content-Type', 'application/javascript');
-        $msg=json_encode($arr );
-        $app->response->body($msg );
+     
         $_SESSION['id'] = $article->id;
         $_SESSION['username'] = $article->username;
+        echo $_SESSION['username'];
           }
       else
       {
